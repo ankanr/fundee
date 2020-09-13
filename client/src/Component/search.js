0bd396ad69455ece578320c74b854602fccb8979
ankanr <zeekozk9@gmail.com>
2020-09-13 09:28:05 +0530

    backend connected to frontend

--- client/src/Component/search.js
+++ client/src/Component/search.js
@@ -4,16 +4,36 @@ import './search.css';
 class SearchBox extends Component {
   constructor(props) {
     super(props);
-    this.state = { search: '', num: '' };
+    this.state = {
+      search: '',
+      val: '',
+      loading: true,
+      name: null,
+      location: null,
+    };
   }
 
-  enteredvalue = ({ target }) => {
-    this.setState({ search: target.value });
+  enteredValue = ({ target }) => {
+    this.setState({
+      search: target.value,
+    });
   };
 
-  printvalue = () => {
-    this.setState({ num: this.state.search });
+  searchValue = () => {
+    this.setState({
+      val: this.state.search,
+      loading: false,
+    });
   };
+
+  async componentDidMount() {
+    const response = await fetch('/ngo/data');
+    const data = await response.json();
+    this.setState({
+      name: data[0].name,
+      location: data[0].location,
+    });
+  }
 
   render() {
     return (
@@ -22,14 +42,22 @@ class SearchBox extends Component {
         <input
           type="text"
           value={this.state.search}
-          onChange={this.enteredvalue}
+          onChange={this.enteredValue}
         />
         <br />
         <br />
-        <button onClick={this.printvalue}>Search</button>
+        <button onClick={this.searchValue}>Search</button>
         <br />
         <br />
-        {this.state.num}
+        {this.state.loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            Name : {this.state.name}
+            <br />
+            Location : {this.state.location}
+          </div>
+        )}
       </div>
     );
   }
